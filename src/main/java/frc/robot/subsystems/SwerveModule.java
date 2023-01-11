88863175a7cff00fd7509e7216d7aa7b6a71c507
realTronsi <ks.yan@yahoo.com>
2023-01-11 12:19:52 -0500

    Negate initial steer encoder position

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -41,7 +41,7 @@ public class SwerveModule extends SubsystemBase {
     driveController.setD(0);
 
     steerEncoder = steerMotor.getEncoder();
-    steerEncoder.setPosition(Math.PI / 2);
+    steerEncoder.setPosition(-Math.PI / 2);
     steerEncoder.setPositionConversionFactor(360.0 / 12.8);
     
     driveEncoder = driveMotor.getEncoder();
