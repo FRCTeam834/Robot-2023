8738001537159230243dcf0d2cf6c22d9d86a6b2
Aditya <arayapudi2006@gmail.com>
2023-03-04 11:49:41 -0500

    Co-authored-by: Nick <NickBull834@users.noreply.github.com> Co-authored-by: sungwon-yu <sungwon-yu@users.noreply.github.com>

---
+++ src/main/java/frc/robot/ShuffleBoard.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+import io.github.oblarg.oblog.annotations.Config;
+
+/** does things and stuff */
+public class ShuffleBoard {
+    
+    @Config.ToggleButton(defaultValue = false, width = 400, height = 400)
+    void enableLEDs(boolean enabled) {
+        
+    }
+
+
+
+    
+
+}
